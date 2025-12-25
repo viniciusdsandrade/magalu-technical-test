6a14766c398edb55e76b26ab92730f4f8df56c52
andrade <viniciusdsandrade0662@gmail.com>
2025-12-25 13:15:36 -0300

    refactor: Channel.kt

--- src/main/kotlin/com/restful/magalu/technical/test/entity/Channel.kt
+++ src/main/kotlin/com/restful/magalu/technical/test/entity/Channel.kt
@@ -1,17 +1,34 @@
 package com.restful.magalu.technical.test.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
 @Entity
-@Table(name = "tb_channel")
-class Channel {
+@Table(
+    name = "tb_channel",
+    uniqueConstraints = [
+        UniqueConstraint(
+            name = "uk_channel_description",
+            columnNames = ["description"]
+        )
+    ]
+)
+open class Channel protected constructor() {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
     var id: Long? = null
-    var description: String? = null
+
+    @Column(name = "description", nullable = false, length = 120)
+    lateinit var description: String
+
+    constructor(description: String) : this() {
+        this.description = description
+    }
 }
