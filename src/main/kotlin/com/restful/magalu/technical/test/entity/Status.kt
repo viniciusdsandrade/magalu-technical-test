969e0fa060b716c77c0f2e284a217bbaa3bff7ef
andrade <viniciusdsandrade0662@gmail.com>
2025-12-25 13:15:48 -0300

    refactor: Status.kt

--- src/main/kotlin/com/restful/magalu/technical/test/entity/Status.kt
+++ src/main/kotlin/com/restful/magalu/technical/test/entity/Status.kt
@@ -1,18 +1,35 @@
 package com.restful.magalu.technical.test.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
 
 @Entity
-@Table(name = "tb_status")
-class Status {
+@Table(
+    name = "tb_status",
+    uniqueConstraints = [
+        UniqueConstraint(
+            name = "uk_status_description",
+            columnNames = ["description"]
+        )
+    ]
+)
+open class Status protected constructor() {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
     var id: Long? = null
-    var description: String? = null
+
+    @Column(name = "description", nullable = false, length = 60)
+    lateinit var description: String
+
+    constructor(description: String) : this() {
+        this.description = description
+    }
 }
