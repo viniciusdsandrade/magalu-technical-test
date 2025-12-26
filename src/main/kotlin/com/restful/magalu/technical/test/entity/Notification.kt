445335fc5af73e3baaa16f23af64678aab076ba1
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2025-12-26 08:50:53 -0300

    refactor: Notification.kt

--- src/main/kotlin/com/restful/magalu/technical/test/entity/Notification.kt
+++ src/main/kotlin/com/restful/magalu/technical/test/entity/Notification.kt
@@ -1,14 +1,12 @@
 package com.restful.magalu.technical.test.entity
 
 import jakarta.persistence.Basic
-import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType.LAZY
 import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
-import jakarta.persistence.Index
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.Lob
 import jakarta.persistence.ManyToOne
@@ -16,31 +14,19 @@ import jakarta.persistence.Table
 import java.time.LocalDateTime
 
 @Entity
-@Table(
-    name = "tb_notification",
-    indexes = [
-        Index(name = "ix_notification_channel_id", columnList = "channel_id"),
-        Index(name = "ix_notification_status_id", columnList = "status_id"),
-        Index(name = "ix_notification_date_time", columnList = "date_time"),
-        Index(name = "ix_notification_destination", columnList = "destination")
-    ]
-)
+@Table(name = "tb_notification")
 open class Notification protected constructor() {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id")
     var id: Long? = null
 
-    @Column(name = "date_time", nullable = false)
     lateinit var dateTime: LocalDateTime
 
-    @Column(name = "destination", nullable = false, length = 255)
     lateinit var destination: String
 
     @Lob
     @Basic
-    @Column(name = "message", nullable = false)
     lateinit var message: String
 
     @ManyToOne(fetch = LAZY, optional = false)
