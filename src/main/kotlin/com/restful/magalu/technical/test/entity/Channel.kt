d9d6cb4128b9bb81ad77bd0fc31fd03bfe13a91b
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2025-12-26 11:32:21 -0300

    refactor: Channel.kt

--- src/main/kotlin/com/restful/magalu/technical/test/entity/Channel.kt
+++ src/main/kotlin/com/restful/magalu/technical/test/entity/Channel.kt
@@ -6,7 +6,11 @@ import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import jakarta.persistence.UniqueConstraint
+import lombok.Getter
+import lombok.Setter
 
+@Getter
+@Setter
 @Entity
 @Table(
     name = "tb_channel",
@@ -20,12 +24,21 @@ import jakarta.persistence.UniqueConstraint
 open class Channel protected constructor() {
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
     var id: Long? = null
 
     lateinit var description: String
 
     constructor(description: String) : this() {
         this.description = description
+    }
+
+    enum class Values(val id: Long, val description: String) {
+        EMAIL(1L, "email"),
+        SMS(2L, "sms"),
+        PUSH(3L, "push"),
+        WHATSAPP(4L, "whatsapp");
+
+        fun toChannel(): Channel =
+            Channel(description).also { it.id = id }
     }
 }
